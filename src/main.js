4e3bd559f3d0c0775273162434288ac98eed9ee8
Cream <marstarty3631@gmail.com>
2023-03-18 01:14:09 +0800

    新增專案

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import './assets/all.scss'
+
+import './assets/custom.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.component('VueLoading',Loading) //全域元件註冊
+app.use(VueAxios, axios)
+
+app.mount('#app')
